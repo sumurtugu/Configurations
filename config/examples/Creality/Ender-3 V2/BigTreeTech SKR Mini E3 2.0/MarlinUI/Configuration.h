cfae353aeef26d6f74f8ad6be0ed7adc4e6e01e0
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-11-15 23:33:17 -0600

    🔧 Fix custom wiring, etc.

--- config/examples/Creality/Ender-3 V2/BigTreeTech SKR Mini E3 2.0/MarlinUI/Configuration.h
+++ config/examples/Creality/Ender-3 V2/BigTreeTech SKR Mini E3 2.0/MarlinUI/Configuration.h
@@ -3495,6 +3495,7 @@
 //#define DWIN_CREALITY_LCD_JYERSUI   // Jyers UI by Jacob Myers
 #define DWIN_MARLINUI_PORTRAIT        // MarlinUI (portrait orientation)
 //#define DWIN_MARLINUI_LANDSCAPE     // MarlinUI (landscape orientation)
+#define NO_CONTROLLER_CUSTOM_WIRING_WARNING
 
 //
 // Touch Screen Settings
